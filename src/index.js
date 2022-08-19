69a60438e2470880c16e706a7e28eb6bcd03ccb6
stanleyawang <stanleyawang@gmail.com>
2022-08-19 20:10:22 +0800

    add auth0 login

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import Modal from "react-modal";
@@ -6,6 +7,12 @@ import Modal from "react-modal";
 Modal.setAppElement("#root");
 
 ReactDOM.render(
-  <App />,
+  <Auth0Provider
+  domain="tws.au.auth0.com"
+  clientId="8tYWJMRaViJVufZRDO7bRyAISKRRQ2BW"
+  redirectUri="https://stanleyawang.github.io/tws"
+>
+  <App /> 
+  </Auth0Provider>,
   document.getElementById("root")
 );
