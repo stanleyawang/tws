01dc1a242f18501065c3845ea56878ee90c710ea
somepcuser <stanleyawang@gmail.com>
2022-08-20 10:44:36 +0800

    update styles

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
 import tw from "twin.macro";
 
-const Wrapper = tw.div`border border-primary-400 mb-4`
-const Header = tw.div`bg-primary-500 text-gray-100 px-4 transform text-base py-3 cursor-pointer`
+const Wrapper = tw.div`border border-primary-400 mb-4 rounded-lg`
+const Header = tw.div`flex h-16 gap-x-5 items-center font-bold text-primary-500 px-6 transform text-base py-3 cursor-pointer`
 const Contents = tw.div``
 const ContentsInner = tw.div`p-4`
 
 const Accordion = (props) => {
 
-  const { title, children } = props
+  const { icon, title, children } = props
 
   const [open, setOpen] = useState(false)
 
   return (
     <Wrapper>
-      <Header onClick={() => setOpen(!open)}>
+      <Header onClick={() => setOpen(!open)}>        
+        { icon }
         {title}
       </Header>
       <Contents style={{ transition: open ? 'max-height 1s ease-in' : 'max-height 0.5s ease-out', overflow: 'hidden', maxHeight: open ? '500px' : '0px' }}>
--- src/demos/QuestionPage.js
+++ src/demos/QuestionPage.js
@@ -4,25 +4,32 @@ import tw from "twin.macro";
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import Footer from "components/footers/FiveColumnWithBackground.js";
 import Header from "components/headers/light.js";
+import { SectionHeading } from "components/misc/Headings.js";
 import Accordion from 'components/accordion/accordion';
+import { ReactComponent as RadioIcon } from "feather-icons/dist/icons/radio.svg";
+import { ReactComponent as CoffeeIcon } from "feather-icons/dist/icons/coffee.svg";
+import { ReactComponent as DropletIcon } from "feather-icons/dist/icons/droplet.svg";
+import { ReactComponent as ZapIcon } from "feather-icons/dist/icons/zap.svg";
 
 const Container = tw.div`relative`;
-const Contents = tw.div`max-w-screen-xl mx-auto py-20 md:py-24`;
-const MainWrapper = tw.div`border border-primary-500`
-const TitleHeader = tw.div`bg-primary-500 text-gray-100 px-4 transform text-3xl md:text-5xl`
+const Contents = tw.div`max-w-screen-xl mx-auto py-10 md:py-10`;
+const TitleHeader = tw(SectionHeading)``;
+const WidthControl = tw.div`md:w-9/12 m-auto`
 const MainContents = tw.div``
 const MainContentsInner = tw.div`p-4 md:p-5`
-const FieldLabel = tw.p`text-base mb-2`
-const Input = tw.input`py-3 text-base text-black w-full h-10 font-normal focus:outline-none border border-primary-500 px-3`
+const FieldLabel = tw.p`font-semibold text-base mb-2 mt-3`
+const Input = tw.input`py-3 text-base text-black w-full h-10 font-normal focus:outline-none border border-primary-500 px-3 rounded-lg`
 const PostCodeBox = tw.div`mb-4`
 const ServiceBox = tw.div``
-
+const Description = tw.p`mb-3`
+const Divider = tw.div`my-8 border-b-2 border-gray-800`
 const Checkbox = tw.div`flex items-center mb-3`
 const CheckboxInput = tw.input`h-4 w-4 border rounded-sm bg-white transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer`
-const CheckboxLabel = tw.label`inline-block text-gray-800 ml-1`
+const CheckboxLabel = tw.label`text-sm inline-block text-gray-800 ml-1`
 
-const AccordionFooter = tw.div`fixed md:relative border-t-1 border-primary-500 bg-white text-black px-4 transform text-base md:text-xl py-3 flex justify-between items-center`
-const PrimaryButton = tw.button`font-bold px-6 lg:px-8 py-3 rounded bg-primary-500 text-gray-100 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
+const AccordionFooter = tw.div`font-semibold fixed border-t-1 border-primary-500 bg-primary-500 text-gray-100 px-4 md:px-64 transform text-xl md:text-xl py-3 flex justify-between items-center`
+const PrimaryButton = tw.button`font-bold px-4 lg:px-8 py-2 rounded bg-secondary-500 text-gray-100 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
+const AttachPhotoButton = tw.button`mt-2 mb-4 font-bold px-3 lg:px-8 py-3 rounded bg-secondary-500 text-gray-100 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
 
 const QuestionPage = () => {
     
@@ -36,65 +43,96 @@ const QuestionPage = () => {
     <AnimationRevealPage>
       <Header />
       <Container>
-        <Contents>
-          <MainWrapper>
+        <Contents>          
             <TitleHeader>
-              Header
+              New Quotation
             </TitleHeader>
-            <MainContents>
-              <MainContentsInner>
-                <PostCodeBox>
-                  <FieldLabel>
-                    Postal Code
-                  </FieldLabel>
-                  <Input placeholder='Postal Code' onChange={(e) => setPostcode(e.target.value)} />
-                </PostCodeBox>
+              <WidthControl>
+                <MainContents>
+                  <MainContentsInner>
+                  <Description>Let us know more about your home. </Description>
+                    <PostCodeBox>
+                      <FieldLabel>
+                        Address
+                      </FieldLabel>
+                      <Input placeholder='96 Bukit Batok Avenue #12-34 Singapore 123456' onChange={(e) => setPostcode(e.target.value)} />
+                    </PostCodeBox>
+                    <Divider />
+                    <ServiceBox>
+                       <FieldLabel>
+                        Services
+                      </FieldLabel>
+                      <Description>Tell us more about the services you need:</Description>
+                      <Accordion title='Cleaning' icon={<RadioIcon />}>
+                      <Checkbox>
+                          <CheckboxInput type="checkbox" value="" id="cleaning-5" />
+                          <CheckboxLabel htmlFor='cleaning-5'>
+                            General Cleaning ($1000)
+                          </CheckboxLabel>  
+                        </Checkbox>
+                        <Checkbox>
+                          <CheckboxInput type="checkbox" value="" id="cleaning-10" />
+                          <CheckboxLabel htmlFor='cleaning-10'>
+                            Deep Cleaning ($1000)
+                          </CheckboxLabel>
+                        </Checkbox>
+                      </Accordion>
 
-                <ServiceBox>
-                  <FieldLabel>
-                    Services
-                  </FieldLabel>
-                  <Accordion title='Cleaning'>
-                    Cleaning contents
-                  </Accordion>
+                      <Accordion title='Painting' icon={<CoffeeIcon />}>
+                      <Checkbox>
+                          <CheckboxInput type="checkbox" value="" id="painting-1" />
+                          <CheckboxLabel htmlFor='painting-1'>
+                            General Painting ($1000)
+                          </CheckboxLabel>
+                        </Checkbox>
+                      </Accordion>
 
-                  <Accordion title='Painting'>
-                    Painting contents
-                  </Accordion>
+                      <Accordion title='Plumbing' icon={<DropletIcon />}>
+                      <Checkbox>
+                          <CheckboxInput type="checkbox" value="" id="plumbing-5" />
+                          <CheckboxLabel htmlFor='plumbing-5'>
+                            Replace Toilet Basin ($100)
+                          </CheckboxLabel>
+                        </Checkbox>
+                      </Accordion>
 
-                  <Accordion title='Plumbing'>
-                    Plumbing contents
-                  </Accordion>
+                      <Accordion title='Electrical' icon={<ZapIcon />}>
+                        <Checkbox>
+                          <CheckboxInput type="checkbox" value="" id="electrical-5" />
+                          <CheckboxLabel htmlFor='electrical-5'>
+                            Change Socket($5)
+                          </CheckboxLabel>
+                        </Checkbox>
 
-                  <Accordion title='Electrical'>
-                    <Checkbox>
-                      <CheckboxInput type="checkbox" value="" id="electrical-5" />
-                      <CheckboxLabel htmlFor='electrical-5'>
-                        Change Socket($5)
-                      </CheckboxLabel>
-                    </Checkbox>
-
-                    <Checkbox>
-                      <CheckboxInput type="checkbox" value="" id="electrical-10" />
-                      <CheckboxLabel htmlFor='electrical-10'>
-                        Change Switch($10)
-                      </CheckboxLabel>
-                    </Checkbox>
-                  </Accordion>
-                </ServiceBox>
-              </MainContentsInner>
-            </MainContents>
-
-            <AccordionFooter className='question-footer'>
-              <p>
-                Total $0
-              </p>
-              <PrimaryButton onClick={handleConfirm}>Confirm</PrimaryButton>
-            </AccordionFooter>
-          </MainWrapper>
+                        <Checkbox>
+                          <CheckboxInput type="checkbox" value="" id="electrical-10" />
+                          <CheckboxLabel htmlFor='electrical-10'>
+                            Change Switch($10)
+                          </CheckboxLabel>
+                        </Checkbox>
+                      </Accordion>
+                    </ServiceBox>
+                    <Divider />
+                    <ServiceBox>
+                       <FieldLabel>
+                        Photos
+                      </FieldLabel>
+                      <Description>Do you have any photos to describe the condition of your place?</Description>
+                      <AttachPhotoButton onClick={handleConfirm}>Add Photo</AttachPhotoButton> 
+                      </ServiceBox>
+                 
+                  </MainContentsInner>
+                </MainContents>
+              </WidthControl>
+              <AccordionFooter className='question-footer'>
+                  <p>
+                    Total:  $0
+                  </p>
+                <PrimaryButton onClick={handleConfirm}>Confirm</PrimaryButton>                
+              </AccordionFooter>          
+     
         </Contents>
-      </Container>
-      <Footer />
+      </Container>      
     </AnimationRevealPage>
   )
 }
